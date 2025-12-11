b05d366fec9a0901e6f62c406f1223e4de37f53d
Akhmatamin <ahmatamin.raimzhanov@alatoo.edu.kg>
2025-12-12 00:00:26 +0600

    CRUD

--- booking_app_fastapi/api/review.py
+++ booking_app_fastapi/api/review.py
@@ -33,3 +33,23 @@ async def create_review(review_data:ReviewInSchema, db:Session = Depends(get_db)
     db.commit()
     db.refresh(review_db)
     return {'message': 'New review created'}
+
+@review_router.put('/{review_id}',response_model=ReviewOutSchema)
+async def update_review(review_id:int, review_data:ReviewInSchema, db:Session = Depends(get_db)):
+    review_db = db.query(Review).filter(Review.id == review_id).first()
+    if review_db is None:
+        raise HTTPException(status_code=400, detail="Review not found")
+    for key, value in review_data.dict().items():
+        setattr(review_db, key, value)
+    db.commit()
+    db.refresh(review_db)
+    return review_db
+
+@review_router.delete('/{review_id}',response_model=dict)
+async def delete_review(review_id:int, db:Session = Depends(get_db)):
+    review_db = db.query(Review).filter(Review.id == review_id).first()
+    if review_db is None:
+        raise HTTPException(status_code=400, detail="Review not found")
+    db.delete(review_db)
+    db.commit()
+    return {'message': 'Review deleted'}
